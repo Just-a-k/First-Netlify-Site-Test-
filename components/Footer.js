fec6bc873fc4fdad37ec781f69dc8295ec206bb6
Just-a-k <80929448+Just-a-k@users.noreply.github.com>
2024-02-21 13:41:04 +1100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,13 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <img src="/Site_Footer (1).svg" alt="Site Logo" className={styles.logo} />
-        <a href="https://71256test.com"></a>
+    <a href="https://71256test.com">
+      <span style="display : block;">
+        <footer className={styles.footer}>
+          <img src="/Site_Footer (1).svg" alt="Site Logo" className={styles.logo} />
+      </span>
       </footer>
+    </a>
     </>
   )
 }
