74ff1f3049ba46d2e41b364104f0a60430b165a1
Just-a-k <80929448+Just-a-k@users.noreply.github.com>
2024-02-22 22:33:17 +0000

    Testing with visual studio

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,13 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-    <a href="https://71256test.com">
+    <a href="https://71256test.netlify.com">
       <span style="display : block;">
         <footer className={styles.footer}>
           <img src="/Site_Footer (1).svg" alt="Site Logo" className={styles.logo} />
+          </footer>
       </span>
-      </footer>
+        
     </a>
     </>
   )
---
+++ pages/Second Page.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>My first site ig</title>
+        <link rel="icon" href="/site_favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Test!" />
+        <p className="description">
+          Go to your Github page (Just-a-k) and go to the "First-Netlify-Site-Test", then <code>pages/index.js</code> to edit this content.
+        </p>
+        <p className='second weblink' />
+          <a href="71256test.netlify.com/test">Second Page</a>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
